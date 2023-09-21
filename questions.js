07e966de5dcc2f11bad67262b4f579802ba362a0
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-09-21 21:56:07 +0100

    added questions

--- questions.js
+++ questions.js
@@ -1,19 +1,54 @@
 const questions = [
-    {
-      questionText: "What is the capital of France?",
-      options: ["Berlin", "Madrid", "Paris", "Rome"],
-      correctOptionIndex: 2,
-    },
-    {
-      questionText: "Which planet is known as the Red Planet?",
-      options: ["Earth", "Venus", "Mars", "Jupiter"],
-      correctOptionIndex: 2,
-    },
-    {
-      questionText: "What Planet are you in",
-      options: ["Earth", "Venus", "Mars", "Jupiter"],
-      correctOptionIndex: 2,
-    },
+  {
+  questionText: "What is StackUp?",
+  options: [ "A social media platform for developers","A learn-to-earn platform for Web3", "A code repository for open source projects"],
+  correctOptionIndex: 1,
+  },
+  {
+  questionText: "What rewards can you earn on StackUp?",
+  options: [ "NFTs", "Access to exclusive events","Cryptocurrency",],
+  correctOptionIndex: 2,
+  },
+  {
+  questionText: "What languages does StackUp offer courses in?",
+  options: ["Ruby", "Javascript", "Golang",],
+  correctOptionIndex: 1,
+  },
+  {
+  questionText: "How can you earn experience points (EXP) on StackUp?",
+  options: ["Completing courses", "Completing campaigns","Completing quests", ],
+  correctOptionIndex: 2,
+  },
+  {
+  questionText: "What are the benefits of using StackUp?",
+  options: ["Connect with other developers and learn from each other","Learn from experts in the field of playing", "Earn Money for enjoyment"],
+  correctOptionIndex: 0,
+  },
+  {
+  questionText: "What is the community on StackUp like?",
+  options: ["Large", "Diverse", "Active"],
+  correctOptionIndex: 1,
+  },
+  {
+  questionText: "What is the future of StackUp?",
+  options: ["To be the leading learn-to-earn platform for Web3", "Not to expand the number of courses and languages offered", "only To partner with more projects and companies in the Web3 space", ],
+  correctOptionIndex: 0,
+  },
+  {
+  questionText: "What are the prerequisites for joining StackUp?",
+  options: ["None, Just have a PC", "A basic understanding of programming with a PC", "A basic understanding of Web3 and a PC"],
+  correctOptionIndex: 0,
+  },
+  {
+  questionText: "What is the best way to get started on StackUp?",
+  options: ["Browse the courses and choose one that interests you", "Complete the onboarding quests", "Join the community and ask questions"],
+  correctOptionIndex: 2,
+  },
+  {
+  questionText: "What are some tips for success on StackUp?",
+  options: ["Be absolutely not consistent with your learning", "You be afraid to ask for help", "Participate in the community","All of the options",],
+  correctOptionIndex: 2,
+  },
   ];
   
   export default questions;
